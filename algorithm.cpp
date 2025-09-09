2f2bf103f805e178248e9394646a9db71eca328d
hamhuo <hamhuo@outlook.com>
2025-09-09 17:42:51 +1200

    finish

--- algorithm.cpp
+++ algorithm.cpp
@@ -221,29 +221,151 @@ string uc_explist(string const initialState, string const goalState,
 // Move Generator:
 //
 ////////////////////////////////////////////////////////////////////////////////////////////
-string aStar_ExpandedList(string const initialState, string const goalState, int &pathLength, int &numOfStateExpansions, int &maxQLength,
-                          float &actualRunningTime, int &numOfDeletionsFromMiddleOfHeap, int &numOfLocalLoopsAvoided, int &numOfAttemptedNodeReExpansions, heuristicFunction heuristic)
+string aStar_ExpandedList(string const initialState, string const goalState,
+                          int &pathLength, int &numOfStateExpansions, int &maxQLength,
+                          float &actualRunningTime, int &numOfDeletionsFromMiddleOfHeap,
+                          int &numOfLocalLoopsAvoided, int &numOfAttemptedNodeReExpansions,
+                          heuristicFunction heuristic)
 {
-
-    string path;
-    clock_t startTime;
-
+    // reset stats
+    pathLength = 0; numOfStateExpansions = 0; maxQLength = 0;
+    actualRunningTime = 0.0f;
     numOfDeletionsFromMiddleOfHeap = 0;
     numOfLocalLoopsAvoided = 0;
     numOfAttemptedNodeReExpansions = 0;
 
-    // cout << "------------------------------" << endl;
-    // cout << "<<aStar_ExpandedList>>" << endl;
-    // cout << "------------------------------" << endl;
-    actualRunningTime = 0.0;
-    startTime = clock();
-    srand(time(NULL));                   // RANDOM NUMBER GENERATOR - ONLY FOR THIS DEMO.  YOU REALLY DON'T NEED THIS! DISABLE THIS STATEMENT.
-    maxQLength = rand() % 200;           // AT THE MOMENT, THIS IS JUST GENERATING SOME DUMMY VALUE.  YOUR ALGORITHM IMPLEMENTATION SHOULD COMPUTE THIS PROPERLY.
-    numOfStateExpansions = rand() % 200; // AT THE MOMENT, THIS IS JUST GENERATING SOME DUMMY VALUE.  YOUR ALGORITHM IMPLEMENTATION SHOULD COMPUTE THIS PROPERLY
+    clock_t startTime = clock();
 
-    //***********************************************************************************************************
-    actualRunningTime = ((float)(clock() - startTime) / CLOCKS_PER_SEC);
-    path = "DDRRLLLUUURDLUDURDLUU"; // this is just a dummy path for testing the function
-    pathLength = path.size();
-    return path;
+    // early exit
+    if (initialState == goalState) {
+        actualRunningTime = float(clock() - startTime) / CLOCKS_PER_SEC;
+        return "";
+    }
+
+    // ---- heuristic helpers (inline) ----
+vector<int> goalPos(10, -1);
+for (int i = 0; i < (int)goalState.size(); i++) {
+    goalPos[goalState[i] - '0'] = i;
+}
+
+    auto calc_h = [&](const string& s)->int {
+    if (heuristic == misplacedTiles) {
+        int cnt = 0;
+        for (int i = 0; i < 9; ++i) {
+            char ch = s[i];
+            if (ch != '0' && ch != goalState[i]) cnt++;
+        }
+        return cnt;
+    } else { // manhattanDistance
+        int sum = 0;
+        for (int i = 0; i < 9; ++i) {
+            char ch = s[i];
+            if (ch == '0') continue;
+            int gi = goalPos[ch - '0'];
+            int r1 = i / 3, c1 = i % 3;
+            int r2 = gi / 3, c2 = gi % 3;
+            sum += abs(r1 - r2) + abs(c1 - c2);
+        }
+        return sum;
+    }
+};
+    // OPEN heap + indexes + CLOSED
+    vector<Node*> openHeap;
+    unordered_map<string, Node*> inOpen;
+    unordered_map<string, int>   bestClosedG;
+
+    // start node
+    Node* start = new Node();
+    start->state = initialState;
+    start->path  = "";
+    start->g = 0;
+    start->h = calc_h(initialState);
+    start->f = start->g + start->h;
+    start->alive = true;
+
+    openHeap.push_back(start);
+    make_heap(openHeap.begin(), openHeap.end(), CmpAstar{});
+    inOpen[start->state] = start;
+    maxQLength = (int)openHeap.size();
+
+    while (!openHeap.empty()) {
+        // pop min-f (tie: larger g first via CmpAstar)
+        pop_heap(openHeap.begin(), openHeap.end(), CmpAstar{});
+        Node* cur = openHeap.back();
+        openHeap.pop_back();
+
+        auto itCurOpen = inOpen.find(cur->state);
+        if (itCurOpen != inOpen.end() && itCurOpen->second == cur) {
+            inOpen.erase(itCurOpen);
+        }
+        if (!cur->alive) {
+            numOfDeletionsFromMiddleOfHeap++;
+            delete cur;
+            continue;
+        }
+
+        if (cur->state == goalState) {
+            string res = std::move(cur->path);
+            pathLength = (int)res.size();
+            actualRunningTime = float(clock() - startTime) / CLOCKS_PER_SEC;
+            delete cur;
+            for (Node* nd : openHeap) delete nd;
+            return res;
+        }
+
+        numOfStateExpansions++;
+        Puzzle curPuzzle(cur->state, goalState);
+        auto succs = successors_URDL(&curPuzzle);
+
+        for (auto &pr : succs) {
+            const string &ns = pr.first;
+            const char    mv = pr.second;
+
+            if (!cur->path.empty() && isInverse(cur->path.back(), mv)) {
+                numOfLocalLoopsAvoided++;
+                continue;
+            }
+
+            const int ng = cur->g + 1;
+            const int nh = calc_h(ns);
+            const int nf = ng + nh;
+
+            auto itCns = bestClosedG.find(ns);
+            if (itCns != bestClosedG.end() && ng >= itCns->second) {
+                numOfAttemptedNodeReExpansions++;
+                continue;
+            }
+
+            auto itOns = inOpen.find(ns);
+            if (itOns != inOpen.end()) {
+                Node* old = itOns->second;
+                if (ng < old->g) {
+                    old->alive = false;  
+                } else {
+                    numOfAttemptedNodeReExpansions++;
+                    continue;
+                }
+            }
+
+            Node* nd = new Node();
+            nd->state = ns;
+            nd->path  = cur->path; nd->path.push_back(mv);
+            nd->g = ng; nd->h = nh; nd->f = nf; nd->alive = true;
+
+            openHeap.push_back(nd);
+            push_heap(openHeap.begin(), openHeap.end(), CmpAstar{});
+            if ((int)openHeap.size() > maxQLength) maxQLength = (int)openHeap.size();
+            inOpen[ns] = nd;
+        }
+
+        auto itCcur = bestClosedG.find(cur->state);
+        if (itCcur == bestClosedG.end() || cur->g < itCcur->second) {
+            bestClosedG[cur->state] = cur->g;
+        }
+        delete cur;
+    }
+
+    actualRunningTime = float(clock() - startTime) / CLOCKS_PER_SEC;
+    for (Node* nd : openHeap) delete nd;
+    return "";
 }
